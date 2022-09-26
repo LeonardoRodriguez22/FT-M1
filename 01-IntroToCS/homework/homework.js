e7d577d386b1e1c256757fa5f5dd9f88e1db15b1
LeonardoRodriguez22 <santiyvalenyleo@gmail.com>
2022-09-26 18:23:56 -0300

    new commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,27 @@
 'use strict'
 
+
 function BinarioADecimal(num) {
   // tu codigo aca
+  var suma = 0
+  
+  for(var i = 0; i < num.length; i++){
+    suma += Math.pow(2, num.length-1-i ) * num[i];
+  }
+  return suma;
+  }
 
-}
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var array = [];
 
+  while(num > 0){
+    array.unshift(num % 2);
+    num = Math.floor(num / 2);
+}
+
+return array.join("");
 }
 
 
