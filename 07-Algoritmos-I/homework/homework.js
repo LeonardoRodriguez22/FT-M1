9ab64b01a75a09923d5441ffd96c4b4b43a8a3b3
LeonardoRodriguez22 <santiyvalenyleo@gmail.com>
2022-10-06 18:10:25 -0300

    leo

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -5,16 +5,40 @@ function factorear(num) {
   // Factorear el número recibido como parámetro y devolver en un array
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
-  // Tu código:
-
+  // // Tu código:
+  var arreglo = [1];
+  var divisor = 2
+  while (num !== 1) {
+    if(num % divisor === 0){
+      arreglo.push(divisor)
+      num = num / divisor
+  }else{
+    divisor = divisor +1;
+  }
 }
+return arreglo;
+}
+  
+
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+var cambio = true
+while(cambio){
+  cambio = false;
+  for (let i = 0; i < array.length - 1; i++) {
+    if(array[i] > array[i+1]){
+      cambio = true
+      var auxiliar = array[i]
+      array[i]= array[i+1]
+      array[i+1]=auxiliar
+    } 
+   }
+}
+return array
 }
 
 
@@ -23,7 +47,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    var  j = i - 1;
+    var auxiliar = array[i];
+    while(j >= 0 && array[j] > auxiliar){
+    array[j +1] = array[j];
+    j--;
+    }
+    array[j+1] = auxiliar
+  }
+  return array;
 }
 
 
@@ -32,6 +65,20 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+for (let i = 0; i  < array.length; i++){
+  var minimo = i;
+  for (let j = i +1; j < array.length; j++) {
+    if(array[j] < array[minimo]){
+      minimo = j
+    }
+  }
+  if(i !== minimo){
+    var auxiliar = array[i];
+    array[i] = array[minimo];
+    array[minimo] = auxiliar;
+  }
+}
+return array
 
 }
 
